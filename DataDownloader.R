d05a304d08ff8f8f63faa037fdb333c7922de771
WillOf-D <102534361+WillOf-D@users.noreply.github.com>
2022-03-30 20:41:19 +0700

    Update DataDownloader.R

--- DataDownloader.R
+++ DataDownloader.R
@@ -7,11 +7,11 @@ drv <- dbDriver("PostgreSQL")
 
 
 con <- dbConnect(drv,
-                 dbname = ELEPHANT_SQL_DBNAME, 
-                 host = ELEPHANT_SQL_HOST, 
+                 dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"), 
+                 host = Sys.getenv("ELEPHANT_SQL_HOST"), 
                  port = 5432,
-                 user = ELEPHANT_SQL_USER, 
-                 password = ELEPHANT_SQL_PASSWORD
+                 user = Sys.getenv("ELEPHANT_SQL_USER"), 
+                 password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
 )
 
 
